c733b4490fa280ee96863eae9b926f432312a208
emmettpeterson <163900722+emmettpeterson@users.noreply.github.com>
2024-04-02 18:05:20 -0400

    Update Visit.sql
    Changed Day to AppointmentDate, added NOT NULl

--- Visit.sql
+++ Visit.sql
@@ -1,9 +1,9 @@
 create table Visit(
 	FOREIGN KEY (PatientID) REFERENCES Patient(PatientID),
 	FOREIGN KEY (DoctorID) REFERENCES Doctor(DoctorID),
-	FOREIGN KEY (Day) REFERENCES Appointment(Day),
-	Diagnosis varchar(500),
-	Treatment varchar(500),
+	FOREIGN KEY (AppointmentDate) REFERENCES Appointment(AppointmentDate),
+	Diagnosis varchar(500) NOT NULL,
+	Treatment varchar(500) NOT NULL,
 	FOREIGN KEY BillID REFERENCES Bill(BillID)
 	PRIMARY KEY (PatientID, DoctorID, Day)
 	);
