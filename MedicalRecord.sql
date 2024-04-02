89d1f587bb31e51e58bf8a4b0e12851cbf06b413
emmettpeterson <163900722+emmettpeterson@users.noreply.github.com>
2024-04-02 18:02:22 -0400

    Update MedicalRecord.sql
    Changed day to appointment date and added not null

--- MedicalRecord.sql
+++ MedicalRecord.sql
@@ -2,5 +2,5 @@ create table MedicalRecord(
 	FOREIGN KEY (PatientID) REFERENCES Patient(PatientID),
 	Diagnosis varchar(500),
 	Treatment varchar(500)
-	Day date
+	AppointmentDate DATE NOT NULL
 	);
