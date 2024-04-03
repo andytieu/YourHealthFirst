4a25174760ee25c2a549b6e3e49ff30c1339880b
andytieu <104803764+andytieu@users.noreply.github.com>
2024-04-02 21:20:47 -0400

    A visit doesn't always yield a treatment

--- Visit.sql
+++ Visit.sql
@@ -3,7 +3,7 @@ create table Visit(
 	FOREIGN KEY (DoctorID) REFERENCES Doctor(DoctorID),
 	FOREIGN KEY (AppointmentDate) REFERENCES Appointment(AppointmentDate),
 	Diagnosis varchar(500) NOT NULL,
-	Treatment varchar(500) NOT NULL,
+	Treatment varchar(500),
 	FOREIGN KEY BillID REFERENCES Bill(BillID)
 	PRIMARY KEY (PatientID, DoctorID, Day)
 	);
